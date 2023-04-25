6e4eace5e04c9e39cff5c27ecc6035d6586f2eab
Den <mirloxd3@gmail.com>
2023-04-25 20:32:29 +0300

    fix: update build scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #/bin/sh
 yarn --cwd ./page build:contract
 
+mkdir -p res
+
 cp ./page/build/release/web4-profile.wasm ./res/contract.wasm
--- build_factory.sh
+++ build_factory.sh
@@ -1,3 +1,5 @@
 RUSTFLAGS='-C link-arg=-s' cargo build --manifest-path=factory/Cargo.toml --target wasm32-unknown-unknown --release
 
+mkdir -p res
+
 cp ./factory/target/wasm32-unknown-unknown/release/factory.wasm ./res/factory.wasm
